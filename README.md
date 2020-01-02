87b8870114582c6857655ec5b38d3d30e62323ff
harrypotter0409 <harry.potter0409@outlook.com>
2020-01-03 00:59:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-registration-login-example
+
+Angular 8 User Registration and Login Example with Webpack 4
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+﻿import './polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
