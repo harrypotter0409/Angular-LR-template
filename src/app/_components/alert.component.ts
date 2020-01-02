94581fd70ae4450b133f5de719f03b2b24ddae6e
harrypotter0409 <harry.potter0409@outlook.com>
2020-01-03 01:36:57 +0800

    model issue

--- src/app/_components/alert.component.ts
+++ src/app/_components/alert.component.ts
@@ -5,28 +5,27 @@ import { AlertService } from '@/_services';
 
 @Component({ selector: 'alert', templateUrl: 'alert.component.html' })
 export class AlertComponent implements OnInit, OnDestroy {
-    private subscription: Subscription;
-    message: any;
+	private subscription: Subscription;
+	message: any;
 
-    constructor(private alertService: AlertService) { }
+	constructor(private alertService: AlertService) {}
 
-    ngOnInit() {
-        this.subscription = this.alertService.getAlert()
-            .subscribe(message => {
-                switch (message && message.type) {
-                    case 'success':
-                        message.cssClass = 'alert alert-success';
-                        break;
-                    case 'error':
-                        message.cssClass = 'alert alert-danger';
-                        break;
-                }
+	ngOnInit() {
+		this.subscription = this.alertService.getAlert().subscribe(message => {
+			switch (message && message.type) {
+				case 'success':
+					message.cssClass = 'alert alert-success';
+					break;
+				case 'error':
+					message.cssClass = 'alert alert-danger';
+					break;
+			}
 
-                this.message = message;
-            });
-    }
+			this.message = message;
+		});
+	}
 
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-    }
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,19 @@ import './_content/app.less';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-    currentUser: User;
+	currentUser: User;
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
+	constructor(
+		private router: Router,
+		private authenticationService: AuthenticationService,
+	) {
+		this.authenticationService.currentUser.subscribe(
+			x => (this.currentUser = x),
+		);
+	}
 
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
+	logout() {
+		this.authenticationService.logout();
+		this.router.navigate(['/login']);
+	}
 }
